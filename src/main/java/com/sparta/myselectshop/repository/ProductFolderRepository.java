abd02a872475b4f726fa731617ab8c3f0ef46ec9
sooooooongyi <sooooooongyi@gmail.com>
2024-10-30 18:00:26 +0900

    feat: 폴더별 관심 상품 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductFolderRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductFolderRepository.java
@@ -3,6 +3,9 @@ package com.sparta.myselectshop.repository;
 import com.sparta.myselectshop.entity.Folder;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.ProductFolder;
+import com.sparta.myselectshop.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
